eaf1220d361f16442166a97106b1717d89b72580
Kévin Commaille <zecakeh@tedomum.fr>
2024-12-12 01:52:01 +0100

    user-profile-dialog: Refactor

--- src/components/dialogs/user_profile.rs
+++ src/components/dialogs/user_profile.rs
@@ -19,9 +19,9 @@ mod imp {
     #[template(resource = "/org/gnome/Fractal/ui/components/dialogs/user_profile.ui")]
     pub struct UserProfileDialog {
         #[template_child]
-        pub stack: TemplateChild<gtk::Stack>,
+        stack: TemplateChild<gtk::Stack>,
         #[template_child]
-        pub user_page: TemplateChild<UserPage>,
+        user_page: TemplateChild<UserPage>,
     }
 
     #[glib::object_subclass]
@@ -32,7 +32,6 @@ mod imp {
 
         fn class_init(klass: &mut Self::Class) {
             Self::bind_template(klass);
-            Self::Type::bind_template_callbacks(klass);
         }
 
         fn instance_init(obj: &InitializingObject<Self>) {
@@ -44,15 +43,37 @@ mod imp {
     impl WidgetImpl for UserProfileDialog {}
     impl AdwDialogImpl for UserProfileDialog {}
     impl ToastableDialogImpl for UserProfileDialog {}
+
+    impl UserProfileDialog {
+        /// Load the user with the given session and user ID.
+        pub(super) fn load_user(&self, session: &Session, user_id: OwnedUserId) {
+            let user = RemoteUser::new(session, user_id);
+            self.user_page.set_user(Some(user.clone()));
+
+            spawn!(clone!(
+                #[weak(rename_to = imp)]
+                self,
+                async move {
+                    user.load_profile().await;
+                    imp.stack.set_visible_child_name("details");
+                }
+            ));
+        }
+
+        /// Set the member to present.
+        pub(super) fn set_room_member(&self, member: Member) {
+            self.user_page.set_user(Some(member.upcast::<User>()));
+            self.stack.set_visible_child_name("details");
+        }
+    }
 }
 
 glib::wrapper! {
-    /// Dialog to join a room.
+    /// Dialog to view a user's profile.
     pub struct UserProfileDialog(ObjectSubclass<imp::UserProfileDialog>)
         @extends gtk::Widget, adw::Dialog, ToastableDialog, @implements gtk::Accessible;
 }
 
-#[gtk::template_callbacks]
 impl UserProfileDialog {
     /// Create a new `UserProfileDialog`.
     pub fn new() -> Self {
@@ -60,27 +81,12 @@ impl UserProfileDialog {
     }
 
     /// Load the user with the given session and user ID.
-    pub fn load_user(&self, session: &Session, user_id: OwnedUserId) {
-        let imp = self.imp();
-
-        let user = RemoteUser::new(session, user_id);
-        imp.user_page.set_user(Some(user.clone()));
-
-        spawn!(clone!(
-            #[weak]
-            imp,
-            async move {
-                user.load_profile().await;
-                imp.stack.set_visible_child_name("details");
-            }
-        ));
+    pub(crate) fn load_user(&self, session: &Session, user_id: OwnedUserId) {
+        self.imp().load_user(session, user_id);
     }
 
     /// Set the member to present.
-    pub fn set_room_member(&self, member: Member) {
-        let imp = self.imp();
-
-        imp.user_page.set_user(Some(member.upcast::<User>()));
-        imp.stack.set_visible_child_name("details");
+    pub(crate) fn set_room_member(&self, member: Member) {
+        self.imp().set_room_member(member);
     }
 }
