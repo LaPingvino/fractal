3a6416f1b3bc469a9bdeaa713bc33dfec8e2adef
Kévin Commaille <zecakeh@tedomum.fr>
2025-04-05 13:16:22 +0200

    failed-session: Refactor

--- src/session_list/failed_session.rs
+++ src/session_list/failed_session.rs
@@ -1,35 +1,19 @@
-use std::{ops::Deref, sync::Arc};
-
-use gtk::{glib, prelude::*, subclass::prelude::*};
+use gtk::{glib, subclass::prelude::*};
 
 use super::{SessionInfo, SessionInfoImpl};
 use crate::{
     components::AvatarData, prelude::*, secret::StoredSession, utils::matrix::ClientSetupError,
 };
 
-#[derive(Clone, Debug, glib::Boxed)]
-#[boxed_type(name = "BoxedClientSetupError")]
-pub struct BoxedClientSetupError(pub Arc<ClientSetupError>);
-
-impl Deref for BoxedClientSetupError {
-    type Target = Arc<ClientSetupError>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
 mod imp {
     use std::cell::OnceCell;
 
     use super::*;
 
-    #[derive(Debug, Default, glib::Properties)]
-    #[properties(wrapper_type = super::FailedSession)]
+    #[derive(Debug, Default)]
     pub struct FailedSession {
         /// The error encountered when initializing the session.
-        #[property(get, construct_only)]
-        error: OnceCell<BoxedClientSetupError>,
+        error: OnceCell<ClientSetupError>,
         /// The data for the avatar representation for this session.
         avatar_data: OnceCell<AvatarData>,
     }
@@ -41,7 +25,6 @@ mod imp {
         type ParentType = SessionInfo;
     }
 
-    #[glib::derived_properties]
     impl ObjectImpl for FailedSession {}
 
     impl SessionInfoImpl for FailedSession {
@@ -55,6 +38,20 @@ mod imp {
                 .clone()
         }
     }
+
+    impl FailedSession {
+        /// Set the error encountered when initializing the session.
+        pub(super) fn set_error(&self, error: ClientSetupError) {
+            self.error
+                .set(error)
+                .expect("error should not be initialized");
+        }
+
+        /// The error encountered when initializing the session.
+        pub(super) fn error(&self) -> &ClientSetupError {
+            self.error.get().expect("error should be initialized")
+        }
+    }
 }
 
 glib::wrapper! {
@@ -65,10 +62,16 @@ glib::wrapper! {
 
 impl FailedSession {
     /// Constructs a new `FailedSession` with the given info and error.
-    pub fn new(stored_session: &StoredSession, error: ClientSetupError) -> Self {
-        glib::Object::builder()
+    pub(crate) fn new(stored_session: &StoredSession, error: ClientSetupError) -> Self {
+        let obj = glib::Object::builder::<Self>()
             .property("info", stored_session)
-            .property("error", BoxedClientSetupError(Arc::new(error)))
-            .build()
+            .build();
+        obj.imp().set_error(error);
+        obj
+    }
+
+    /// The error encountered when initializing the session.
+    pub(crate) fn error(&self) -> &ClientSetupError {
+        self.imp().error()
     }
 }
